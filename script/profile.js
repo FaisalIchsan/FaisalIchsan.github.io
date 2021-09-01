18bf74a2fd37f013425bf0c23e18054ae94c7d5d
FaisalIchsan <76278048+FaisalIchsan@users.noreply.github.com>
2021-09-01 15:08:23 +0700

    Update profile.js

--- script/profile.js
+++ script/profile.js
@@ -1,144 +1,46 @@
-<html lang="ID">
-<head>
-    <title>Portofolio Faisal Nur Ichsan</title>
-     <!--- Metadata Standard -->
-    <meta charset="utf-8">
-    <title>CeeVee - Free Responsive HTML5/CSS3 Template</title>
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <!-- Metadata untuk tampilan  Mobile  -->
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-
-     <!-- Metadata untuk google fonts     -->
-     <link rel="preconnect" href="https://fonts.gstatic.com">
-     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet">
-
-    <!-- Metadata untuk CSS  -->
-    <link rel="stylesheet" href="script/profile.css">
-
-    <!-- Metadata Favicons  -->
-    <link rel="shortcut icon" href="favicon.ico" >
-</head>
-<body > <!-- start body -->
-<img src="image/wallpaper.jpg" class="background-image">
-<img src="image/wallpaper.jpg" class="background-image background-animated-2">
-<img src="image/wallpaper.jpg" class="background-image background-animated">
-<div id="main"> <!-- start konten -->
-    <!-- start section profile -->
-    <figure id="foto-profil">
-        <img src="image/profil.png">
-    </figure>
-    <div class="bio-konten">
-        <header> <!-- start header -->
-        <h1 class="nama-profil">Faisal Nur Ichsan </h1>
-        <nav>
-            <a href="#home" class="menunav" data-menu="home">home</a>
-            <a href="#pendidikan" class="menunav" data-menu="pendidikan">pendidikan</a>
-            <a href="#biografi" class="menunav" data-menu="biografi">biografi</a>
-            <a href="#contact" class="menunav" data-menu="contact">contact</a>
-            
-        </nav>
-        </header> <!-- end header -->
-        <ul id="biografi" class="list list-hide">
-                <p class="biografi">Saya mahasiswa aktif semester 5 dengan basic keilmuan Ilmu Perpustakaan..</p>
-                
-        </ul>
-        <ul id="biografi" class="list">
-<p class="home"></p>
-
-        </ul>
-        <ul id="contact" class="list list-hide">
-<p class="contact">Email : faisalnurichan12@gmail.com 
-</p>
-
-        </ul>
-        <ul id="home" class="list list-hide">
-            <p class="home">Selamat Datang di Website Pribadi Faisalichsan. Have u Enjoyed!!
-            </p>
-
-        </ul>
-        
-        <ul id="pendidikan" class="list list-hide">
-            <li class="kolom-list">
-                <date class="list-tahun">2007-2013</date>
-                <div>
-                    <p class="list-judul">Sekolah Dasar</p>
-                    <p class="list-keterangan">
-                        SD Negeri Cipedak 05
-                    <p>
-                </div>
-            </li>
-            <li class="kolom-list">
-                <date class="list-tahun">2013-2016</date>
-                <div >
-                    <p class="list-judul">Sekolah Menengah Pertama</p>
-                    <p class="list-keterangan">SMP Negeri 131 Jakarta</p>
-                </div>
-            </li>
-            <li class="kolom-list">
-                <date class="list-tahun">2016-2019</date>
-                <div >
-                    <p class="list-judul">Sekolah Menengah Atas</p>
-                    <p class="list-keterangan">SMA Negeri 6 Depok</p>
-                </div>
-            </li>
-            <li class="kolom-list">
-                <date class="list-tahun">2019</date>
-                <div>
-                    <p class="list-judul">Strata 1</p>
-                    <p class="list-keterangan">
-                        Mahasiswa Aktif Semester 5 UIN Syarif Hidayatullah Jakarta Prodi Ilmu Perpustakaan
-                    </p>    
-                </div>
-            </li>
-        </ul>
-        
-        <footer> <!-- start footer -->
-            <div id="download-resume">
-                <a target="_blank" class="download-btn" href="Faisal nur ichsan_CV.pdf" download>
-                    <i class="ti-download"></i>
-                    <p> Unduh Resume</p>
-                </a>
-            </div>
-            <ul id="sosial-media">
-                <li>
-                    <a target="_blank" class="kontainer-sosmed" href="www.gmail.com/">
-                        <img class="icon-sosmed" src="image/kisspng-ipma-email-telephone-message-email-icon-5acf36458d39e5.4305648115235292855785.png" alt="email">
-                    </a>                    
-                </li>
-                
-                <li>
-                    <a target="_blank" class="kontainer-sosmed" href="https://www.instagram.com/faisalnurichsan/">
-                        <img class="icon-sosmed" src="image/instagram..png" alt="instagram">
-                    </a>                    
-                </li>
-                <li>
-                    <a target="_blank" class="kontainer-sosmed" href="https://www.linkedin.com/in/faisal-nur-ichsan-1881b8218/">
-                        <img class="icon-sosmed" src="linkedin.png" alt="linkedin">
-                    </a>                    
-                </li>
-                <li>
-                    <a target="_blank" class="kontainer-sosmed" href="faisalichsan.github.io/">
-                        <img class="icon-sosmed" src="image/github-logo.png" alt="github">
-                    </a>                    
-                </li>
-            </ul>
-        </footer> <!-- end footer -->    
-    </div>
-</div> <!-- end konten -->
-<!-- Metadata untuk script   -->
-<script src="script/profile.js"></script>
-<!-- Contact Section
-	Author   : Al Muhdil Karim
+/*----------------------------------------------------*/
+/*	Author   : Al Muhdil Karim
     Site     : https://almudhilkarim.com
     github   : https://github.com/jasonvortech/
     telegram : @rocknroot
     this template is used for learning purposes on basic web development on karims library 
     https://almuhdilkarim.com
- -->
-</body> <!-- end body -->
-</html>
+------------------------------------------------------ */
+
+//=============================
+// Algoritma fungsi utama
+//=============================
+
+// Proses-1. Menetapkan konstanta menu navigasi dengan menggunakan nama kelas
+const menunav =  document.getElementsByClassName("menunav");
+
+// Proses-2. Menunggu event, jika element yang mempunyai class menunav di click lalu m,engeksekusi fungction 'transit'
+for (var i = 0; i < menunav.length; i++) {
+    menunav[i].addEventListener('click', transit, false);
+}
+
+//=============================
+// Algoritma function transit
+//=============================
+function transit() {
+    // Proses-1. menetapkan variable dengan setiap elemen yang mempunyai class css 'list'
+    let getclass =  document.getElementsByClassName("list");
+
+    // Proses-2. Memanipulasi setiap element class 'list'
+    for (var i = 0; i < getclass.length; i++) {
+        // 2.1 Menghapus seluruh class 'list-show' yang tedapat didalam semua tag yang mempunyai class 'list' 
+        getclass[i].classList.remove("list-show");
+        // 2.1 menambahkan class 'list-show' pada semua element yang mempunyai class 'list''
+        getclass[i].classList.add("list-hide");
+    }
+
+    // Proses-3. Mengambil attribut data menu untuk mendapatkan id class yang ingin dimanipulasi
+    let getid = this.getAttribute("data-menu");
+    // Proses-4. Menghapus class 'last-hide' dari id yang didapatkan dalam proses ke 3
+    document.getElementById(getid).classList.remove("list-hide");
+    // Proses-5. Menambahkan class 'last-show' dari id yang didapatkan dalam proses ke 3
+    document.getElementById(getid).classList.add("list-show");
+}
 
   
 
